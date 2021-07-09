c3947c9a939f0bb29cfeedd7d4829560d12326e3
Jose Maria Alvarez Martinez <jose.ma.fime@gmail.com>
2021-07-08 21:50:10 -0500

    pruebas

--- Backend/AdmonBD/AdmonBD/Servicios/ConexionBD.cs
+++ Backend/AdmonBD/AdmonBD/Servicios/ConexionBD.cs
@@ -2,10 +2,12 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using System.Data.SqlClient;
 
 namespace AdmonBD.Servicios
 {
     public class ConexionBD
     {
+
     }
 }
