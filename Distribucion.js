af4b814b81797304c51e4f75e612cf436b9f93df
Jose Maria Alvarez Martinez <jose.ma.fime@gmail.com>
2021-07-12 00:30:57 -0500

    js con metodos para frontend

--- Distribucion.js
+++ Distribucion.js
@@ -1,4 +1,4 @@
-const uri = 'https://localhost:44390/api/ClaveMateria';
+const uri = 'https://localhost:44390/api/Distribucion';
 
 //FUNCIONES PARA CONSULTAR INDIVIDUAL Y TODOS LOS ELEMENTOS
 //Pendiente modificar los ElementByID porque faltan referencias
@@ -15,7 +15,7 @@ function _displayItems(data){
 }
 
 function ConsultaPorId(){
-    let id = document.getElementById("nombreclave").value;
+    let id = document.getElementById("").value;
     let url = uri + '/'+ id;
     fetch(url)
     .then(response => response.json())
@@ -33,22 +33,36 @@ function llenarCampos(data){
 //FUNCIONES PARA AGREGAR UN ITEM EN LA BASE DE DATOS
 //Pendiente modificar los ElementByID porque faltan referencias
 function Agregar(){
-    const inputDescripcion = document.getElementById("descripcion").value;
-    const inputNombreClave = document.getElementById("nombreclave").value;
-    objClaveMateria = {
-        "Descripcion": inputDescripcion,
-        "NombreClave": inputNombreClave
+    const inputIdDistribucion = document.getElementById("").value;
+    const inputIdPlan = document.getElementById("").value;
+    const inputIdClavemateria = document.getElementById("").value;
+    const inputIdMateria = document.getElementById("").value;
+    const inputIdGrupo = document.getElementById("").value;
+    const inputIdHora = document.getElementById("").value;
+    const inputIdDia = document.getElementById("").value;
+    const inputIdSalon = document.getElementById("").value;
+    const inputSemestre = document.getElementById("").value;
+    obj = {
+        "IdDistribucion": inputIdDistribucion,
+        "IdPlan": inputIdPlan,
+        "IdClavemateria": inputIdClavemateria,
+        "IdMateria": inputIdMateria,
+        "IdGrupo": inputIdGrupo,
+        "IdHora": inputIdHora,
+        "IdDia": inputIdDia,
+        "IdSalon": inputIdSalon,
+        "Semestre": inputSemestre
     }
     fetch(uri,{
         method: 'POST',
         headers:{
             'Content-Type': 'application/json'
         },
-        body: JSON.stringify(objClaveMateria)
+        body: JSON.stringify(obj)
     })
     .then(response => response.text())
     .then(data => Mensaje(data))
-    .catch(error => console.error('Unable to Agregar Item.',error ,objClaveMateria));
+    .catch(error => console.error('Unable to Agregar Item.',error));
 }
 //----------------------------------------------------------------------------------------------------------------------------------------------------
 //FUNCION PARA IMPRIMIR MENSAJE DE ERROR O DE EXITO
@@ -59,8 +73,8 @@ function Mensaje(data){
 //FUNCIONES PARA ELIMINAR UN ITEM EN LA BASE DE DATOS
 //Pendiente modificar los ElementByID porque faltan referencias
 function Eliminar(){
-    const inputIdClavemateria = document.getElementById("nombreclave").value;
-    let url = uri + "/" + inputIdClavemateria;
+    const inputIdClavemateria = document.getElementById("").value;
+    let url = uri + "/" + inputIdClavemateria
     fetch(url,{method:'DELETE'})
     .then(response => response.text())
     .then(data => Mensaje(data))
@@ -69,20 +83,34 @@ function Eliminar(){
 //----------------------------------------------------------------------------------------------------------------------------------------------------
 //FUNCION PARA EDITAR UN ITEM EN LA BASE DE DATOS
 function Editar(){
-    const inputDescripcion = document.getElementById("descripcion").value;
-    const inputNombreClave = document.getElementById("nombreclave").value;
-    objClaveMateria = {
-        "Descripcion": inputDescripcion,
-        "NombreClave": inputNombreClave
+    const inputIdDistribucion = document.getElementById("").value;
+    const inputIdPlan = document.getElementById("").value;
+    const inputIdClavemateria = document.getElementById("").value;
+    const inputIdMateria = document.getElementById("").value;
+    const inputIdGrupo = document.getElementById("").value;
+    const inputIdHora = document.getElementById("").value;
+    const inputIdDia = document.getElementById("").value;
+    const inputIdSalon = document.getElementById("").value;
+    const inputSemestre = document.getElementById("").value;
+    obj = {
+        "IdDistribucion": inputIdDistribucion,
+        "IdPlan": inputIdPlan,
+        "IdClavemateria": inputIdClavemateria,
+        "IdMateria": inputIdMateria,
+        "IdGrupo": inputIdGrupo,
+        "IdHora": inputIdHora,
+        "IdDia": inputIdDia,
+        "IdSalon": inputIdSalon,
+        "Semestre": inputSemestre
     }
     fetch(uri,{
         method: 'PUT',
         headers:{
             'Content-Type': 'application/json'
         },
-        body: JSON.stringify(objClaveMateria)
+        body: JSON.stringify(obj)
     })
     .then(response => response.text())
     .then(data => Mensaje(data))
-    .catch(error => console.error('Unable to Editar Item.',error ,objClaveMateria));
+    .catch(error => console.error('Unable to Editar Item.',error));
 }
