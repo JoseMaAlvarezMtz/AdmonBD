a9437715b22a9603d3091347afe79670a6ebb22f
Jose Maria Alvarez Martinez <jose.ma.fime@gmail.com>
2021-07-11 19:28:44 -0500

    controllers para conexion de front con back

---
+++ Backend/AdmonBD/AdmonBD/Controllers/PlanEstudiosController.cs
@@ -0,0 +1,83 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using AdmonBD.Models;
+
+namespace AdmonBD.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class ClaveMateriaController : ControllerBase
+    {
+        private readonly AdmonContext context;
+        private string objeto = "CLAVE DE MATERIA";
+
+        public ClaveMateriaController(AdmonContext admonContext)
+        {
+            this.context = admonContext;
+        }
+
+        [HttpGet]
+        public IEnumerable<ClaveMateria> Get()
+        {
+
+            return context.ClaveMateria.ToList();
+        }
+
+        [HttpGet("{id}")]
+        public ClaveMateria Get(string id)
+        {
+
+            return context.ClaveMateria.FirstOrDefault( j => j.NombreClave == id);
+        }
+
+        [HttpPost]
+        public string Post(ClaveMateria claveMateria)
+        {
+            try
+            {
+                context.ClaveMateria.Add(claveMateria);
+                context.SaveChanges();
+            }
+            catch (Exception ex)
+            {
+                return ex.Message;
+            }
+            return objeto + " agregada satisfactoriamente";
+        }
+
+        [HttpPut]
+        public string Put(ClaveMateria claveMateria)
+        {
+            try
+            {
+                var Editar = context.ClaveMateria.FirstOrDefault(j => j.NombreClave == claveMateria.NombreClave);
+                Editar.Descripcion = claveMateria.Descripcion;
+                context.SaveChanges();
+            }
+            catch(Exception ex)
+            {
+                return ex.Message;
+            }
+            return objeto + " actualizada satisfactoriamente";
+        }
+
+        [HttpDelete("{id}")]
+        public string Delete(string id)
+        {
+            try
+            {
+                context.Remove(context.ClaveMateria.Single(j => j.NombreClave == id));
+                context.SaveChanges();
+            }
+            catch(Exception ex)
+            {
+                return ex.Message;
+            }
+            return objeto + " borrada satisfactoriamente";
+        }
+    }
+}
---
+++ ClaveMateria.js
@@ -0,0 +1,88 @@
+const uri = 'https://localhost:44390/api/ClaveMateria';
+
+//FUNCIONES PARA CONSULTAR INDIVIDUAL Y TODOS LOS ELEMENTOS
+//Pendiente modificar los ElementByID porque faltan referencias
+function getItems() {
+  fetch(uri)
+    .then(response => response.json())
+    .then(data => _displayItems(data))
+    .catch(error => console.error('Unable to get items.', error));
+}
+
+function _displayItems(data){
+    console.log(data);
+    //PENDIENTE DE TERMINAR
+}
+
+function ConsultaPorId(){
+    let id = document.getElementById("nombreclave").value;
+    let url = uri + '/'+ id;
+    fetch(url)
+    .then(response => response.json())
+    .then(data => llenarCampos(data))
+    .catch(error => console.error('Unable to Consulta Por Id', error));
+}
+
+function llenarCampos(data){
+    console.log(data.nombreClave);
+    console.log(data.idClavemateria);
+    console.log(data.descripcion);
+    //PENDIENTE DE TERMINAR
+}
+//-------------------------------------------------------------------------------------------------------------------------------------------------
+//FUNCIONES PARA AGREGAR UN ITEM EN LA BASE DE DATOS
+//Pendiente modificar los ElementByID porque faltan referencias
+function Agregar(){
+    const inputDescripcion = document.getElementById("descripcion").value;
+    const inputNombreClave = document.getElementById("nombreclave").value;
+    objClaveMateria = {
+        "Descripcion": inputDescripcion,
+        "NombreClave": inputNombreClave
+    }
+    fetch(uri,{
+        method: 'POST',
+        headers:{
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify(objClaveMateria)
+    })
+    .then(response => response.text())
+    .then(data => Mensaje(data))
+    .catch(error => console.error('Unable to Agregar Item.',error ,objClaveMateria));
+}
+//----------------------------------------------------------------------------------------------------------------------------------------------------
+//FUNCION PARA IMPRIMIR MENSAJE DE ERROR O DE EXITO
+function Mensaje(data){
+    console.log(data);
+}
+//----------------------------------------------------------------------------------------------------------------------------------------------------
+//FUNCIONES PARA ELIMINAR UN ITEM EN LA BASE DE DATOS
+//Pendiente modificar los ElementByID porque faltan referencias
+function Eliminar(){
+    const inputIdClavemateria = document.getElementById("nombreclave").value;
+    let url = uri + "/" + inputIdClavemateria
+    fetch(url,{method:'DELETE'})
+    .then(response => response.text())
+    .then(data => Mensaje(data))
+    .catch(error => console.error('Unable to Eliminar Item',error));
+}
+//----------------------------------------------------------------------------------------------------------------------------------------------------
+//FUNCION PARA EDITAR UN ITEM EN LA BASE DE DATOS
+function Editar(){
+    const inputDescripcion = document.getElementById("descripcion").value;
+    const inputNombreClave = document.getElementById("nombreclave").value;
+    objClaveMateria = {
+        "Descripcion": inputDescripcion,
+        "NombreClave": inputNombreClave
+    }
+    fetch(uri,{
+        method: 'PUT',
+        headers:{
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify(objClaveMateria)
+    })
+    .then(response => response.text())
+    .then(data => Mensaje(data))
+    .catch(error => console.error('Unable to Editar Item.',error ,objClaveMateria));
+}
